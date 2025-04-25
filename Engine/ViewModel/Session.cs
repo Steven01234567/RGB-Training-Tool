d662f5d73ceb940f6a96536fd5c157e3e41c0d71
Steven01234567 <steven.bin.chen17@gmail.com>
2025-04-25 19:48:30 -0400

    Added viewmodel

---
+++ Engine/ViewModel/Session.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.ViewModel
+{
+    public class Session
+    {
+        /// 
+        /// Properties
+        /// 
+        private static readonly Random _generator = new Random();
+
+        public Colour CurrentColourGuess { get; set; }
+        public Colour CurrentColourTarget { get; set; }
+
+
+        /// 
+        /// Constructor
+        /// 
+        public Session()
+        {
+            CurrentColourGuess = new Colour();
+            CurrentColourTarget = GenerateRandomColour();
+        }
+
+
+        ///
+        /// Methods
+        ///
+        public Colour GenerateRandomColour(int minRed = 0, int maxRed = 255,
+                                           int minGreen = 0, int maxGreen = 255,
+                                           int minBlue = 0, int maxBlue = 255)
+        {
+            return new Colour(_generator.Next(minRed, maxRed + 1),
+                              _generator.Next(minGreen, maxGreen + 1),
+                              _generator.Next(minBlue, maxBlue + 1));
+        }
+    }
+}
